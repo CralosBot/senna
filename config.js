aa1b315a24002e8494db3802602d46f1cf322740
Carlos_ vps <95423489+CralosBot@users.noreply.github.com>
2023-12-25 15:11:19 -0500

    Update config.js

--- config.js
+++ config.js
@@ -3,12 +3,12 @@ import chalk from 'chalk'
 import { fileURLToPath } from 'url'
 
 global.owner = [
-  ['59172945992', 'FG98', true],
-  ['5491168352204']
+  ['593961745744', 'FG98', true],
+  ['593961745744']
 ] //Numeros de owner 
 
 global.mods = [''] 
-global.prems = ['50489079501', '573143917092']
+global.prems = ['593961745744', '593961745744']
 global.APIs = { // API Prefix
   // name: 'https://website' 
   nrtm: 'https://fg-nrtm.ddns.net',
@@ -20,22 +20,22 @@ global.APIKeys = { // APIKey Here
 }
 
 // Sticker WM
-global.packname = 'Senna┃ᴮᴼᵀ' 
+global.packname = 'Mordecai┃ᴮᴼᵀ' 
 global.author = '@fg98' 
 
 //--info FG
 global.botName = 'Senna'
-global.fgig = 'https://www.instagram.com/fg98_ff' 
-global.fgsc = 'https://github.com/FG98F/dylux-fg' 
-global.fgyt = 'https://youtube.com/fg98f'
-global.fgpyp = 'https://paypal.me/fg98f'
+global.fgig = 'N/A' 
+global.fgsc = 'https://github.com/CarlosCCG23/BotWa' 
+global.fgyt = 'https://www.youtube.com/@ChumoGH'
+global.fgpyp = 'N/A'
 global.fglog = 'https://i.ibb.co/1zdz2j3/logo.jpgs' 
 
 //--- Grupos WA
-global.fgcanal = 'https://whatsapp.com/channel/0029VaCeuZd6mYPQiWqxXj1F'
-global.bgp = 'https://chat.whatsapp.com/BESBo5xjvIZE4YVvth6Yzr'
-global.bgp2 = 'https://chat.whatsapp.com/I7bvd8XCAOUHjgkHteqFC7'
-global.bgp3 = 'https://chat.whatsapp.com/F0JTTyZ3hsoL7OlU8TEpuH' //--GP NSFW
+global.fgcanal = 'https://chat.whatsapp.com/GrTATxi2ZAZBWAgpH0ODLP'
+global.bgp = 'https://chat.whatsapp.com/BQFoaDlEX8s1MsK9uk4bUo'
+global.bgp2 = 'https://chat.whatsapp.com/Kh8QYTDVC4oHlNYBPmFCkx'
+global.bgp3 = 'N/A' //--GP NSFW
 
 global.wait = '⌛ _Cargando..._\n*▬▬▬▭*'
 global.rwait = '⌛'
